9e55db22d255a9cb1fd4516aa929aac677e72223
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-07-31 20:18:29 -0400

    third commit

---
+++ src/components/Stopwatch.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+
+class Stopwatch extends Component {
+        render() {
+          
+          return (
+            <div className= "Stopwatch">
+                
+                <p id = "clock">
+                  
+                </p>
+                <div>
+                    <button>Reset</button>
+                    <button>Start</button>
+                    <button>Pause</button>
+                </div>
+            </div>
+          )
+        }
+    }
+    export default Stopwatch;
+    
+    
+   
